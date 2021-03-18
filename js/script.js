ee0a45c96a42322d68de3bec8a8421ba1422629b
Benito <bbxblack@gmail.com>
2021-03-18 17:14:53 +0100

    v0.9

--- js/script.js
+++ js/script.js
@@ -1,39 +1,11 @@
 // Creare un hamburger menu con l'utilizzo di Html, Css e JavaScript.
 //  Scrivere solo la parte con jQuery senza modiﬁcare HTML e CSS.
 
-// --- CLICK
-    // JS
-    // document.getElementById('text')
-    //     .addEventListener('click', function() {
-    //         textClick('js click');
-    //     });
-    // !!!WRONG!!! JS/JQ !!!WRONG!!!
-    // $('#text')
-    //     .addEventListener('click', function() {
-    //         textClick('js click');
-    //     });
-    // JQ
-    // $('#text').click(function() {
-    //     textClick('jq click');
-    // });
-    // --- HOVER
-    // JQ -- VERS EXT
-    // $('#text').hover(function() {
-    //     console.log('mouse ENTER');
-    //     $('#title').addClass('text-yellow');
-    // }, function() {
-    //     console.log('mouse LEAVER');
-    //     $('#title').removeClass('text-yellow');
-    // });
-    // // JQ -- VERS COMPR.
-    // $('#text').hover(function() {
-    //     console.log('hover');
-    //     $('#title').toggleClass('text-yellow');
-    // });
-    // --- ANIM
-    // $('#title').hide(5000, function() {
-    //     $('#title').show(1000);
-    // });
-    // $('#title').fadeOut(5000, function() {
-    //     $('#title').fadeIn(1000);
-    // });
+
+$('.fa-bars').click(function() {
+  $('.hamburger-menu').css('display', 'block');
+});
+
+$('.fa-times').click(function() {
+  $('.hamburger-menu').css('display', 'none');
+});
